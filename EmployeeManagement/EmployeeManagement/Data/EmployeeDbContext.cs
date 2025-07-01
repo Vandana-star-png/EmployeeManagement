73c086ffb773706409e9ef9ca83067ce7cda148c
Vandana-star-png <vandanacreddy6543@gmail.com>
2025-07-01 16:26:34 +0530

    Created connection string in appsettings and Added connection to program.cs

--- EmployeeManagement/EmployeeManagement/Data/EmployeeDbContext.cs
+++ EmployeeManagement/EmployeeManagement/Data/EmployeeDbContext.cs
@@ -11,5 +11,6 @@ namespace EmployeeManagement.Data
         }
 
         public DbSet<Employees> Employees { get; set; }
+
     }
 }
--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,6 +1,12 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<EmployeeDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
